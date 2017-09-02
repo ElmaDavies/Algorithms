ae2127c22169c830989d888b8a59c634c069906f
mook <mhd188@yeah.net>
2017-09-02 09:26:44 +0800

    insert sort with pyhon

---
+++ insert_sort.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+#插入排序,升序排序python版
+'''
+书中已给出了伪代码
+简单的插入排序，建立一个循环不变式，并确定一个key值，
+不断地与key之前的数据进行比较，并交换，之后不断移动
+key的值，知道key的值移动到列表末尾。
+
+'''
+a1 = [1,3,2,6,4,2,5,9,7]
+j = 1
+n = len(a1)
+k = 0
+while j < len(a1):
+    key = a1[j]
+    i = j-1
+    while i>0 and a1[i]>key:
+        a1[i+1] = a1[i]
+        i = i-1
+    a1[i+1] = key
+    j = j+1
+    
+for k in range(0,n,1):
+    print a1[k]
+
+   
