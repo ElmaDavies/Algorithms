cf54209ebd1a633345345ccaaaa6336df9fa2195
mook <mhd188@yeah.net>
2017-09-03 17:11:34 +0800

    merge sort with python

---
+++ megreSort.py
@@ -0,0 +1,43 @@
+#coding:utf-8
+'''
+归并排序
+算法导论
+递归，分治的时，自己调用自己
+'''
+
+def merge(a,b):
+    c = []
+    h = j =0
+    while h < len(a) and j < len(b):
+        if a[h] < b[j]:
+            c.append(a[h])
+            h = h+1
+        else:
+            c.append(b[j])
+            j = j+1
+
+    """当a，b不相等时，完成剩下的某个数组排列 """
+    if j == len(b):
+        for i in a[h:]:
+            c.append(i)
+    else:
+        for i in b[j:]:
+            c.append(i)
+    
+    return c
+            
+def mergeSort(lists):
+    if len(lists) <= 1:
+        
+        return lists
+    middle = len(lists)/2
+    left = mergeSort(lists[:middle])
+    right = mergeSort(lists[middle:])
+    return merge(left,right)
+if __name__ == '__main__':
+    lists = [1,4,5,2,6,2,4]
+    
+    print mergeSort(lists)
+    
+
+ 
